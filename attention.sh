4bc560f386a8b7927e871f0768c0e430db694841
Mina Kamel <minallkamel@gmail.com>
2025-05-05 17:43:32 +0300

    get it to start the program, shows notifications and run in the desktop file

--- attention.sh
+++ attention.sh
@@ -8,10 +8,32 @@ last_screen_blanking_state="on"
 last_fullscreen_state="no_fullscreen"
 
 
-if [ -z "$app_name" ]; then
+if [[ -z "$app_name" ]]; then
 	echo "You did't provide a program to track"
 	exit 1
 fi
+
+launch_app() {
+    "$app_name" &> /dev/null &
+    script_pid=$!
+}
+
+wait_for_window_to_show_up() {
+    while ! wmctrl -lp | grep -q "$script_pid"; do
+        sleep 0.2
+    done
+}
+
+is_window_closed() {
+    if ! wmctrl -lp | grep -q "$script_pid"; then
+        echo "$app_name window is gone.."
+        echo "shutting down."
+        exit 0
+    fi
+}
+
+launch_app
+wait_for_window_to_show_up
 
 app_id=$(wmctrl -l | grep -ie "$app_name" | awk '{print $0}')
 
@@ -23,6 +45,7 @@ check_if_fullscreen() {
 turn_off_screen_blanking() {
     if [ "$last_screen_blanking_state" == "on" ]; then
         echo "Turning screen blanking off.."
+        notify-send "⚠️ Power Inhibited" "Your system will not sleep while $app_name is running."
         xset -dpms  # disable DPMS (Energy Star) aka power manager
         last_screen_blanking_state="off"
     fi
@@ -31,12 +54,14 @@ turn_off_screen_blanking() {
 turn_on_screen_blanking() {
     if [ "$last_screen_blanking_state" == "off" ]; then
         echo "Turning screen blanking on.."
+        notify-send "⚠️ Power Management is back to normal"
         xset +dpms
         last_screen_blanking_state="on"
     fi
 }
 
 while true; do
+    is_window_closed
     if [ "$(check_if_fullscreen)" == "0" ]; then
         if [ "$last_fullscreen_state" == "no_fullscreen" ]; then
             echo "$app_name is now fullscreen.."
