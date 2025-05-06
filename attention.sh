5e0647d56f8307dbd474dd1251aa6d5d118a0b7d
Mina Kamel <minallkamel@gmail.com>
2025-05-07 01:51:47 +0300

    allow the user to choose between fullscreen or audio

--- attention.sh
+++ attention.sh
@@ -1,17 +1,40 @@
 #!/usr/bin/bash
 
-# metadata
-app_name="$1"
+HELP="
+    attention <flags> <app_name>
+    --track-fullscreen  Disable power management if the app is fullscreen
+    --track-audio       Disable power management if the app is playing audio and assuming video
+"
+
+app_name=""
+track_fullscreen="false"
+track_audio="false"
+
+if [[ "$#" -ne "2" ]]; then
+    echo "$HELP"
+    exit 1
+fi
+
+for arg in "$@"; do
+    if [[ "$arg" == "--track-fullscreen" ]]; then
+        echo "We are tracking fullscreen.."
+        track_fullscreen="true"
+    elif [[ "$arg" == "--track-audio" ]]; then
+        echo "We are tracking audio.."
+        track_audio="true"
+    else
+        app_name="$arg"
+    fi
+done    
 
 # flags
 last_screen_blanking_state="on"
 last_fullscreen_state="no_fullscreen"
+last_track_audio_state="no"
 
-
-if [[ -z "$app_name" ]]; then
-	echo "You did't provide a program to track"
-	exit 1
-fi
+create_entry_box() {
+    yad --entry 
+}
 
 launch_app() {
     "$app_name" &> /dev/null &
@@ -37,15 +60,34 @@ wait_for_window_to_show_up
 
 app_id=$(wmctrl -l | grep -ie "$app_name" | awk '{print $0}')
 
-check_if_fullscreen() { 
+is_fullscreen() { 
     is_it_full_screen=$(xprop -id "$app_id" | grep -ie "_NET_WM_STATE(ATOM) = _NET_WM_STATE_FULLSCREEN")
     echo "$?"
+}
+
+is_playing_audio() {
+    result=""
+    audio_outputs_registered=$(pactl list sink-inputs)
+    does_the_app_registering_an_audio_output=$(echo "$audio_outputs_registered" | grep -ie "$app_name")
+    result+="$?"
+    
+    is_the_audio_player_open=$(echo "$audio_outputs_registered" | grep -ie "stream.is-live = \"true\"")
+    result+="$?"
+    
+    is_the_audio_playing=$(echo "$audio_outputs_registered" | grep -ie "Corked: no")
+    result+="$?"
+    
+    if [[ "$result" == "000" ]]; then
+        echo "0"
+    else
+        echo "1"
+    fi
 }
 
 turn_off_screen_blanking() {
     if [ "$last_screen_blanking_state" == "on" ]; then
         echo "Turning screen blanking off.."
-        notify-send "⚠️ Power Inhibited" "Your system will not sleep while $app_name is running."
+        notify-send "⚠️ Power Management is inhibited by $app_name"
         xset -dpms  # disable DPMS (Energy Star) aka power manager
         last_screen_blanking_state="off"
     fi
@@ -60,20 +102,46 @@ turn_on_screen_blanking() {
     fi
 }
 
-while true; do
-    is_window_closed
-    if [ "$(check_if_fullscreen)" == "0" ]; then
-        if [ "$last_fullscreen_state" == "no_fullscreen" ]; then
+we_are_tracking_fullscreen() {
+
+    if [[ "$(is_fullscreen)" == "0" ]]; then
+        if [[ "$last_fullscreen_state" == "no_fullscreen" ]]; then
             echo "$app_name is now fullscreen.."
             last_fullscreen_state="fullscreen"
             turn_off_screen_blanking
         fi
    else
-       if [ "$last_fullscreen_state" == "fullscreen" ]; then
-           turn_on_screen_blanking
-           echo "Traking $app_name for fullscreen.."
+       if [[ "$last_fullscreen_state" == "fullscreen" ]]; then
+           echo "$app_name is no longer fullscreen.."
            last_fullscreen_state="no_fullscreen"
+           turn_on_screen_blanking
        fi
+    fi 
+}
+
+we_are_tracking_audio() {
+    
+    if [[ "$(is_playing_audio)" == "0" ]]; then
+        if [[ "$last_track_audio_state" == "no" ]]; then
+            echo "$app_name is now playing audio.."
+            last_track_audio_state="yes"
+            turn_off_screen_blanking
+        fi
+   else
+       if [[ "$last_track_audio_state" == "yes" ]]; then
+           turn_on_screen_blanking
+           echo "$app_name is no longer playing audio.."
+           last_track_audio_state="no"
+       fi
+    fi 
+}
+
+while true; do
+    is_window_closed
+    if [[ "$track_fullscreen" == "true" ]]; then
+        we_are_tracking_fullscreen
+    elif [[ "$track_audio" == "true" ]]; then
+        we_are_tracking_audio
     fi
-    sleep 1
+   sleep 1
 done
